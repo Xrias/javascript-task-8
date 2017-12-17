8d70158627503393b70ebf319a3291ad32b7c6e0
Reshetnikov Artem <mr.artem.reshetnikov.2017@mail.ru>
2017-12-18 02:19:16 +0500

    Update client-core.js

--- client-core.js
+++ client-core.js
@@ -60,7 +60,7 @@ function LIST(args) {
     };
 
     return requestPromise(options)
-        .then(messages => messages.map(message => paintCommands(message, args.v)),
+        .then(messages => messages.map(message => paintCommands(message)),
             err => console.error(err))
         .then(messages => messages.join('\n\n'), err => console.error(err));
 }
@@ -78,7 +78,7 @@ function SEND(args) {
     };
 
     return requestPromise(options)
-        .then(message => paintCommands(message, args.v), err => console.error(err));
+        .then(message => paintCommands(message), err => console.error(err));
 }
 
 /** Удаляем сообщение
